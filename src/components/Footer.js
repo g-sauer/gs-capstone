c92ce2c475fb551cd84d536bc9de3abf88cf4641
g-sauer <118859174+g-sauer@users.noreply.github.com>
2023-03-17 20:07:36 -0300

    import logos, add meta tags and og tags, set up header nav and footer

---
+++ src/components/Footer.js
@@ -0,0 +1,46 @@
+export const Footer = () => {
+  return (
+    <footer>
+      <img src={'footer-logo.png'} alt='Little Lemon logo' />
+      <nav>
+        <header>Navigation</header>
+        <ul>
+          <li>
+            <a href=''>Home</a>
+          </li>
+          <li>
+            <a href=''>About</a>
+          </li>
+          <li>
+            <a href=''>Menu</a>
+          </li>
+          <li>
+            <a href=''>Reservations</a>
+          </li>
+          <li>
+            <a href=''>Order Online</a>
+          </li>
+          <li>
+            <a href=''>Login</a>
+          </li>
+        </ul>
+      </nav>
+      <nav>
+        <header>Contact</header>
+        <ul>
+          <li>Address</li>
+          <li>Phone number</li>
+          <li>Email</li>
+        </ul>
+        <nav>
+          <header>Social media</header>
+          <ul>
+            <li>Twitter</li>
+            <li>Facebook</li>
+            <li>Instagram</li>
+          </ul>
+        </nav>
+      </nav>
+    </footer>
+  )
+}
---
+++ src/components/Header.js
@@ -0,0 +1,7 @@
+export const Header = () => {
+  return (
+    <header>
+      <img src={"Logo.svg"} alt="Little Lemon logo" />
+    </header>
+  )
+}
