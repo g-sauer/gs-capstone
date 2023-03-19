a7a4d92368b010cd18dbcda02bb8a9d6161e2ee6
g-sauer <118859174+g-sauer@users.noreply.github.com>
2023-03-19 13:10:16 -0300

    Refactor specials

--- src/components/Main.js
+++ src/components/Main.js
@@ -12,55 +12,71 @@ const Card = ({ image, name, price, text }) => (
     </div>
   </div>
 )
+
+const Hero = () => (
+  <main>
+    <div className='hero'>
+      <div>
+        <h1>Little Lemon</h1>
+        <h2>Chicago</h2>
+        <main>
+          We are a family owned <br /> Mediterranean restaurant, <br />
+          focused on traditional <br /> recipes served with a modern <br />
+          twist.
+        </main>
+        <a className='button' href='/reserve'>
+          Reserve a Table
+        </a>
+      </div>
+      <img src={'restauranfood.jpg'} alt='' />
+    </div>
+  </main>
+)
+
+const Specials = () => {
+  const specials = [
+    {
+      image: 'greek-salad.jpg',
+      name: 'Greek salad',
+      price: '12.99',
+      text: 'The famous Greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons.',
+    },
+    {
+      image: 'bruschetta.svg',
+      name: 'Bruschetta',
+      price: '5.99',
+      text: 'Our Bruschetta is made from grilled bread that has been smeared with garlic and seasoned with salt and olive oil. ',
+    },
+    {
+      image: 'lemon-dessert.jpg',
+      name: 'Lemon Dessert',
+      price: '5.00',
+      text: 'This comes straight from grandma’s recipe book, every last ingredient has been sourced and is as authentic as can be imagined.',
+    },
+  ]
+  return (
+    <div className='specials'>
+      <h1>This week's specials!</h1>
+      <a className='button' href='/menu'>
+        Online Menu
+      </a>
+      {specials.map((special) => (
+        <Card
+          image={special.image}
+          name={special.name}
+          price={special.price}
+          text={special.text}
+        />
+      ))}
+    </div>
+  )
+}
+
 export const Main = () => {
   return (
-    <main>
-      <div className='hero'>
-        <div>
-          <h1>Little Lemon</h1>
-          <h2>Chicago</h2>
-          <main>
-            We are a family owned <br /> Mediterranean restaurant, <br />
-            focused on traditional <br /> recipes served with a modern <br />
-            twist.
-          </main>
-          <a className='button' href='/reserve'>
-            Reserve a Table
-          </a>
-        </div>
-        <img src={'restauranfood.jpg'} alt='' />
-      </div>
-
-      <div className='specials'>
-        <h1>This week's specials!</h1>
-        <a className='button' href='/menu'>
-          Online Menu
-        </a>
-        <Card
-          image={'greek-salad.jpg'}
-          name={'Greek salad'}
-          price={'12.99'}
-          text={
-            'The famous Greek salad of crispy lettuce, peppers, olives and our Chicago style feta cheese, garnished with crunchy garlic and rosemary croutons.'
-          }
-        />
-        <Card
-          image={'bruschetta.svg'}
-          name={'Bruschetta'}
-          price={'5.99'}
-          text={
-            'Our Bruschetta is made from grilled bread that has been smeared with garlic and seasoned with salt and olive oil. '
-          }
-        />
-        <Card
-          image={'lemon-dessert.jpg'}
-          name={'Lemon Dessert'}
-          price={'5.00'}
-          text={
-            'This comes straight from grandma’s recipe book, every last ingredient has been sourced and is as authentic as can be imagined.'
-          }
-        />
-      </div>
-    </main>
+    <>
+      <Hero />
+      <Specials />
+    </>
   )
 }
